4a92066f19027db0563a783d7dbf848eae7a24a5
Yochem van Rosmalen <yochem+git@icloud.com>
2023-01-31 15:18:30 +0100

    Refactor

--- lua/autosplit.lua
+++ lua/autosplit.lua
@@ -1,44 +1,31 @@
 local M = {}
 
 local function autosplit(args)
-	local count = args.count
+	local split_args = { range = { args.count } }
 
-	local split_args = { range = { count } }
-	if args.args ~= '' then
-		split_args[1] = args.args
+	-- only add if non-empty because giving an empty string as filename errors
+	local filename = args.args
+	if filename ~= '' then
+		split_args[1] = filename
 	end
 
-	local winwidth = vim.fn.winwidth(0)
-	local winheight = vim.fn.winheight(0)
+	local win_height = vim.api.nvim_win_get_height(0)
+	local win_width = vim.api.nvim_win_get_width(0)
 
-	local vsplit = function()
-		-- default is splitting the window in half
-		if count == 0 then
-			split_args.range = {math.floor(winwidth / 2)}
-		end
-		vim.cmd.vsplit(split_args)
+	local split_cmd = "split"
+
+	-- if auto and there fits another vertical window
+	if M.config.split == "auto" and win_width > 2 * M.config.min_win_width then
+		split_cmd = "v" .. split_cmd
 	end
 
-	local split = function()
-		-- default is splitting the window in half
-		if count == 0 then
-			split_args.range = {math.floor(winheight / 2)}
-		end
-		vim.cmd.split(split_args)
+	-- default is splitting the window in half
+	if split_args.range[1] == 0 then
+		local winsize = split_cmd:sub(1, 1) == "v" and win_width or win_height
+		split_args.range[1] = math.floor(winsize / 2)
 	end
 
-	if M.config.split == "auto" then
-		-- if there fits another vertical window
-		if winwidth > 2 * M.config.min_win_width then
-			vsplit()
-		else
-			split()
-		end
-	elseif M.config.split == "horizontal" then
-		split()
-	elseif M.config.split == "vertical" then
-		vsplit()
-	end
+	vim.cmd[split_cmd](split_args)
 end
 
 function M.setup(options)
